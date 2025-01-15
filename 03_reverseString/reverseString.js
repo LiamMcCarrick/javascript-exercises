2aacd0b80b0ee688e87a0aa09eb92d93d29a88ff
Liam McCarrick <129102185+LiamMcCarrick@users.noreply.github.com>
2025-01-15 13:38:48 -0500

    solve problem 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const stringArray = [];
+    for (let char of string) {
+        stringArray.push(char);
+    }
 
+    stringArray.reverse();
+    
+    let revString = "";
+
+    for (let char of stringArray) {
+        revString += char;
+    }
+
+    return revString;
 };
 
 // Do not edit below this line
