d7f0a08c45795db940ee14e656eb2dca2f805682
Liam McCarrick <129102185+LiamMcCarrick@users.noreply.github.com>
2025-01-15 13:38:00 -0500

    solve problems 3 and 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    /*
+    let args = Array.prototype.slice.call(arguments,1);
+    let filteredArrary = array;
+
+    for (let arg of args){
+        filteredArrary = filteredArrary.filter(val => val !== arg);
+    }
+
+    return filteredArrary */
+    const newArray = [];
+    array.forEach((item) => {
+        if (!args.includes(item)){
+            newArray.push(item)
+        }
+    });
+    return newArray;
 };
 
+console.log(removeFromArray([1,2,3,4],2,3));
+
 // Do not edit below this line
-module.exports = removeFromArray;
+//module.exports = removeFromArray;
