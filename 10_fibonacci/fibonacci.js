74fc0b53b171e09a3b5dfeab3d532523c919631b
Liam McCarrick <129102185+LiamMcCarrick@users.noreply.github.com>
2025-01-21 13:33:02 -0500

    solve question 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(fibNum) {
+    if (fibNum < 0) {
+        return "OOPS"
+    } else if (fibNum == 0) {
+        return 0;
+    } else if (fibNum == 1) {
+        return 1;
+    }
+
+    fibNum = Number(fibNum);
+    let fib = 0;
+    let prev2 = 0;
+    let prev1 = 1;
+    for (i = 2; i <= fibNum; i++) {
+        fib = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = fib;
+    }
+    return fib;
 
 };
 
