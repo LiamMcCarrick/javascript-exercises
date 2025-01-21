f8544254c9c83693858cd4853dab330d5bd97d42
Liam McCarrick <129102185+LiamMcCarrick@users.noreply.github.com>
2025-01-21 12:43:42 -0500

    solve question 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const palinArr = [];
+    for (char of str) {
+        palinArr.push(char.toLowerCase());
+    }
+    const palinFilter = palinArr.filter((char) => {
+        if ((char.charCodeAt(0) > 47 && char.charCodeAt(0) < 58) ||
+        (char.charCodeAt(0) > 64 && char.charCodeAt(0) < 91) ||
+        (char.charCodeAt(0) > 96 && char.charCodeAt(0) < 123)  ) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+    const palinStrFilter = palinFilter.toString();
 
+    const reverseStr = palinFilter.reverse().toString();
+
+    return palinStrFilter === reverseStr;
 };
 
 // Do not edit below this line
