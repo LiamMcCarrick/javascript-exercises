936a6f9dfb95a2d4337eca83a8f1f44471a0e635
Liam McCarrick <129102185+LiamMcCarrick@users.noreply.github.com>
2025-01-21 12:08:10 -0500

    solve question 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(numberOne,numberTwo) {
+	return numberOne + numberTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function(numberOne,numberTwo) {
+	return numberOne - numberTwo;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length === 0) {
+    return 0;
+  }
+	const arrSum = array.reduce((total,currentNum) => total + currentNum);
+  return arrSum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  arrMult = array.reduce((total,currentNum) => total * currentNum);
+  return arrMult;
 };
 
-const power = function() {
-	
+const power = function(numberOne,numberTwo) {
+  return numberOne ** numberTwo;
 };
 
-const factorial = function() {
-	
+const factorial = function(factNum) {
+	const factArr = [];
+  if (factNum === 0) {
+    return 1;
+  }
+  for (i = 1; i <= factNum; i++) {
+    factArr.push(i);
+  }
+  factArr.reverse();
+  return factArr.reduce((total, currentNum) => total * currentNum);
 };
 
 // Do not edit below this line
